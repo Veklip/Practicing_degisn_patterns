efd12f739511ac4d6c1ed0c1a8a191da49a9ed66
kkk <492217330@qq.com>
2015-09-15 11:44:33 +0800

    add singleton

---
+++ Singleton Pattern/src/SingletonPatternDemo.java
@@ -0,0 +1,14 @@
+
+public class SingletonPatternDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingleObject single = SingleObject.getInstance();
+		
+		single.showMsg();
+	}
+
+}

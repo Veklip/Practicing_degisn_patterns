bcadc5cc19d83d472a62c923c1a93ddc8b4439c2
kkk <492217330@qq.com>
2015-09-29 11:42:22 +0800

    add intercepting filter pattern

---
+++ Intercepting Filter Pattern/src/InterceptingFilterDemo.java
@@ -0,0 +1,18 @@
+
+public class InterceptingFilterDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FilterManager filterManager = new FilterManager(new Target());
+		filterManager.setFilter(new AuthenticationFilter());
+		filterManager.setFilter(new DebugFilter());
+
+		Client client = new Client();
+		client.setFilterManager(filterManager);
+		client.sendRequest("HOME");
+	}
+
+}
